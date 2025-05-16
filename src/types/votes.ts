4448095341e1eebd476fc793ef00a13a5e9fc0c5
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-05-16 22:55:57 +0300

    component App and CafeInfo

--- src/types/votes.ts
+++ src/types/votes.ts
@@ -1,1 +1,8 @@
 // Загальні типи, які використовуються в кількох компонентах,
+export type VoteType = "good" | "neutral" | "bad";
+
+export interface Votes {
+  good: number;
+  neutral: number;
+  bad: number;
+}
